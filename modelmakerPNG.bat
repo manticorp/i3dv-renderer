4ff95ad6b9bf1e1c2c511b20b0a1be8c9d409ba5
Harry Mustoe-Playfair <manticorp1234@gmail.com>
2013-06-19 21:38:57 +0200

    Modelmaker wasn't working with certain folder paths, now it is

--- modelmakerPNG.bat
+++ modelmakerPNG.bat
@@ -3,21 +3,28 @@ IF "%2"=="" (set /p color="What colour would you like the background (format #AB
 IF "%color%"=="" (set color="#00FF00")
 echo Color equals %color%
 @Echo OFF
-Set "AbsolutePath=%1"
+Set "AbsolutePath=%1%"
+for /f "delims=" %%A in (%AbsolutePath%) do (
+    set modelid=%%~nxA
+)
+Echo Folder 1 : %modelid%
+IF "%modelid%"=="" (GOTO NoFolder) ELSE (GOTO GotFolder)
+:NoFolder
 set "AbsolutePath=%AbsolutePath:\= %"
 FOR %%# in (%AbsolutePath%) do (
-    Set "LastFolder=%%#"
+    Set "modelid=%%#"
 )
-Echo Folder: %LastFolder%
+Echo Folder 2 : %modelid%
+:GotFolder
 md %1\videos
 mogrify -background "%color%" -flatten -verbose -format jpg %1\*.png
-copy /y %1\%LastFolder%0149.jpg %1\videos\thumb.jpg
-ffmpeg -y -f image2 -r 25 -i %1\%LastFolder%%%04d.jpg -c:v libvpx  -g 36 -b:v 1.5M -r 25 -s 960x960 %1\videos\960.webm
-ffmpeg -y -f image2 -r 25 -i %1\%LastFolder%%%04d.jpg -c:v libx264 -g 36 -b:v 1.5M -r 25 -s 960x960 %1\videos\960.mp4
-ffmpeg -y -f image2 -r 25 -i %1\%LastFolder%%%04d.jpg -c:v libvpx  -g 36 -b:v 1M   -r 25 -s 720x720 %1\videos\720.webm
-ffmpeg -y -f image2 -r 25 -i %1\%LastFolder%%%04d.jpg -c:v libx264 -g 36 -b:v 1M   -r 25 -s 720x720 %1\videos\720.mp4
-ffmpeg -y -f image2 -r 25 -i %1\%LastFolder%%%04d.jpg -c:v libvpx  -g 36 -b:v 0.5M -r 25 -s 360x360 %1\videos\360.webm
-ffmpeg -y -f image2 -r 25 -i %1\%LastFolder%%%04d.jpg -c:v libx264 -g 36 -b:v 0.5M -r 25 -s 360x360 %1\videos\360.mp4
+copy /y %1\%modelid%0149.jpg %1\videos\thumb.jpg
+ffmpeg -y -f image2 -r 25 -i %1\%modelid%%%04d.jpg -c:v libvpx  -g 36 -b:v 1.5M -r 25 -s 960x960 %1\videos\960.webm
+ffmpeg -y -f image2 -r 25 -i %1\%modelid%%%04d.jpg -c:v libx264 -g 36 -b:v 1.5M -r 25 -s 960x960 %1\videos\960.mp4
+ffmpeg -y -f image2 -r 25 -i %1\%modelid%%%04d.jpg -c:v libvpx  -g 36 -b:v 1M   -r 25 -s 720x720 %1\videos\720.webm
+ffmpeg -y -f image2 -r 25 -i %1\%modelid%%%04d.jpg -c:v libx264 -g 36 -b:v 1M   -r 25 -s 720x720 %1\videos\720.mp4
+ffmpeg -y -f image2 -r 25 -i %1\%modelid%%%04d.jpg -c:v libvpx  -g 36 -b:v 0.5M -r 25 -s 360x360 %1\videos\360.webm
+ffmpeg -y -f image2 -r 25 -i %1\%modelid%%%04d.jpg -c:v libx264 -g 36 -b:v 0.5M -r 25 -s 360x360 %1\videos\360.mp4
 md %1\sprites
 md %1\sprites\960
 Echo Now doing montage...
